665c7bab18ac9e65aa7f5e02a7132727a214e0bd
Tuany Cristo <93205336+TuanyCristo@users.noreply.github.com>
2024-10-24 10:37:30 -0300

    Adição conheça-nos - SEM IMPORT NO APP

--- projeto1/src/App.js
+++ projeto1/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar/Navbar';
 import Footer from './Footer';
+//import ConhecaNos from './Section2/conhecanos'; // Importe o componente
+
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {<Footer/>}
+      <Footer />
     </div>
   );
 }
